3b667e7adee61a17b815cdf860d62ab04362de9b
Zac Stowell <zgstowell@gmail.com>
2020-03-24 13:53:22 -0700

    counter working

--- breathe.js
+++ breathe.js
@@ -1,15 +1,76 @@
 var breatheBtn = $("#breathe");
 var timerSpan = $("#timer")
 
-breatheBtn.on("click", function () {
+var counterBool = true;
+var counterInt;
+breatheBtn.on("click", countUp);
 
-    var myVar = setInterval(myTimer, 1300);
 
-    function myTimer() {
-        var d = new Date();
-        for (i=1; i<10; i++){
-            
+// function myTimer() {
+//     var timer = setInterval(timer, 1300);
+
+//     // !** MAIN BREATH TIMER **! //
+//     var counter = 0;
+
+//     // COUNTER COUNTS AND LINKS TO TEXT ON PAGE
+//     function timer() {
+
+//         do {
+//             if (counter < 5) {
+//                 counter++;
+//                 timerSpan.text(counter);
+//             } else {
+//                 clearInterval();
+//                 counterBool = false;
+//             }
+//         } while (counterBool === true)
+
+//         do {
+//             if (counter > 0) {
+//                 counter--;
+//                 timerSpan.text(counter);
+//             } else {
+//                 counterBook = true;
+//             }
+//         } while (counterBool === false);
+
+
+//     }
+
+// }
+var counter = 0;
+function countUp() {
+    var counterInt = setInterval(timer, 1300);
+
+   
+
+    function timer() {
+        if (counter <4) {
+            counter++;
+            console.log(counter);
+            timerSpan.text(counter);
+
+        } else {
+            clearInterval(counterInt);
+            countDwn();
         }
-        timerSpan.text
     }
-})
+}
+
+function countDwn() {
+    var counterInt = setInterval(timer, 1300);
+
+    function timer() {
+        if (counter > 0){
+            counter--;
+            console.log(counter);
+            timerSpan.text(counter);
+        } else {
+            clearInterval(counterInt);
+            countUp();
+        }
+}
+
+}
+
+// define function for counter outside of event then functions call each other
