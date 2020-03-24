d2dc5e464deef5093b3219fece56068fe356a819
michael Zuniga <michaelzunigaa@gmail.com>
2020-03-24 14:27:44 -0700

    video-gameapi

--- script.js
+++ script.js
@@ -43,12 +43,44 @@ var localNews="http://newsapi.org/v2/everything?q=Seattle&apiKey=d775fffd012c44e
 
 
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid&api-key=AexBs82F271plW5NYllpAgocdxd0Q2Dw"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid&api-key=AexBs82F271plW5NYllpAgocdxd0Q2Dw";
 
    $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response){
-        console.log(response)
+        console.log(response);
     });
+
+    
+
+
+// var videoGameKey="658189deaf2d44e2844c10f9832acbcc8f09ee43";
+var videoGameUrl="https://api.rawg.io/api/games?dates=2020-03-03,2020-10-10&ordering=-added";
+
+$.ajax({
+  url: videoGameUrl,
+  method:"GET"
+}).then(function(response){
+  console.log(response);
+  var newRelease = response.results;
+  for (var i = 0; i < newRelease.length; i++) {
+    var list= newRelease[i].name;
+    console.log(list);
+    
+    console.log(newRelease[i]);
+
+    $
+
+  }
+    
+  });
+  
+    
+  
+  
+
+
+
+// });
 
