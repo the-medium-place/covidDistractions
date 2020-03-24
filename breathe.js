50d8dddc3cbb7a1352d304144411edd680212457
Zac Stowell <zgstowell@gmail.com>
2020-03-24 10:39:21 -0700

    main pages

---
+++ breathe.js
@@ -0,0 +1,15 @@
+var breatheBtn = $("#breathe");
+var timerSpan = $("#timer")
+
+breatheBtn.on("click", function () {
+
+    var myVar = setInterval(myTimer, 1300);
+
+    function myTimer() {
+        var d = new Date();
+        for (i=1; i<10; i++){
+            
+        }
+        timerSpan.text
+    }
+})
