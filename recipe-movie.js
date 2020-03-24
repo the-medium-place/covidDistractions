c066850ed87661a1f726f87400da9969436321cc
Devin Heigert <heigertd@gmail.com>
2020-03-24 10:17:11 -0700

    more apis

--- recipe-movie.js
+++ recipe-movie.js
@@ -1,58 +1,4 @@
-
-
-
-// top News articles in the US//
-var newsUrl ="http://newsapi.org/v2/top-headlines?country=us&apiKey=d775fffd012c44ec8001a6ace97e7e1f";
-$.ajax({
-    url: newsUrl,
-    method: "GET"
-  }).then(function (response) {
-      console.log(response);
-
-  });
-
-
-
-//news about the covid-19//
-var covidNews= "http://newsapi.org/v2/everything?q=covid19&apiKey=d775fffd012c44ec8001a6ace97e7e1f";
-$.ajax({
-    url: covidNews,
-    method: "GET"
-  }).then(function (response) {
-      console.log(response);
-
-  });
-
-
-
-
-//seattle news//
-var localNews="http://newsapi.org/v2/everything?q=Seattle&apiKey=d775fffd012c44ec8001a6ace97e7e1f";
-
-
- 
-  $.ajax({
-    url: localNews,
-    method: "GET"
-  }).then(function (response) {
-      console.log(response);
-
-  });
-
-
-
-
-
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid&api-key=AexBs82F271plW5NYllpAgocdxd0Q2Dw"
-
-   $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response){
-        console.log(response)
-    });
-
-    //meant to be user inputs
+//meant to be user inputs
 var ingredientOne = "banana"; 
 var ingredientTwo = "tomato";
 var ingredientThree = "milk";
@@ -82,5 +28,3 @@ $.ajax({
         console.log(response.Rated); // grabs rating
         console.log(response.Plot); // grabs plot
     });
-
-    
