7c149d6ffb416896eec537aef843bf38d5979068
Devin Heigert <heigertd@gmail.com>
2020-03-24 10:11:12 -0700

    more apis

--- recipe-movie.js
+++ recipe-movie.js
@@ -4,18 +4,10 @@ var ingredientTwo = "tomato";
 var ingredientThree = "milk";
 var movie = 'titanic';
 
-var queryURLnytimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101&end_date=20120101&api-key=AexBs82F271plW5NYllpAgocdxd0Q2Dw&fl=abstract"
-var queryURLmovie = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
+var queryURLnytimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?fq=romney&facet_field=day_of_week&facet=true&begin_date=20120101var queryURLmovie = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
 var queryURLrecipes = "https://api.edamam.com/search?q=" + ingredientOne + "," + ingredientTwo + "," + ingredientThree + "&app_id=e051d977&app_key=8185235f4070e8bff9a51c85891e2ca1&from=0&to=3&calories=591-722&health=alcohol-free"
 
 
-    //able to pull the information but it wont log the path to abstratct
-   $.ajax({
-        url: queryURLnytimes, //ny times ajax
-        method: "GET"
-    }).then(function (response){
-         console.log(response.docs[0].abstract)
-    });
 
     $.ajax({
         url: queryURLrecipes, //recipe ajax
