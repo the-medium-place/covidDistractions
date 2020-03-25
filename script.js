1e7f9efc642423081cc1183110a3a6c4cea6ec2d
michael Zuniga <michaelzunigaa@gmail.com>
2020-03-25 10:17:26 -0700

    script-update

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=covid
 
 
 // var videoGameKey="658189deaf2d44e2844c10f9832acbcc8f09ee43";
-var videoGameUrl="https://api.rawg.io/api/games?dates=2020-03-03,2020-10-10&ordering=-added";
+var videoGameUrl="https://api.rawg.io/api/games?dates=2020-01-01,2020-03-25&ordering=-added";
 
 $.ajax({
   url: videoGameUrl,
@@ -64,13 +64,21 @@ $.ajax({
 }).then(function(response){
   console.log(response);
   var newRelease = response.results;
-  for (var i = 0; i < newRelease.length; i++) {
-    var list= newRelease[i].name;
-    console.log(list);
+  for (var i = 0; i < 10; i++) {
+    var gameName="Title: " + newRelease[i].name;
+    var gameConsole= "Console: "+ newRelease[i].platforms[i].platform[0];
+    var gamePic=newRelease[i].background_image;
+    var gameRating= "Rating: " + newRelease[i].rating;
+
+    $("<ul>").append(gameName);
+    console.log(gamePic);
+    console.log(gameName);
+    console.log(gameConsole);
+    console.log(gameRating);
     
     console.log(newRelease[i]);
 
-    $
+    // $("<ul>")
 
   }
     
