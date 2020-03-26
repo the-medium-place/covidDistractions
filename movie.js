c05808bc4f6a4e74793bd4bb5dec40023cee5a4e
bbtwots <brettbelka@gmail.com>
2020-03-25 17:54:49 -0700

    movie interface pretty well finished. needs formatting

--- movie.js
+++ movie.js
@@ -1,35 +1,52 @@
-var movie = 'titanic';
+var movie = 'Goodfellas'
+var searchBTN = $("#search");
+var searchFieldEl = $("#search-input");
+var imdbEL = $("#IMDB");
+var streamEL = $("#stream");
 
+
+//button click
+//clear content for subsequent search
+$("#search").on("click", function () {
+    event.preventDefault();
+    movie = searchFieldEl.val();
+    MDBQuery();
+    streamQuery();
+});
+
+//query IMDB by title
 function MDBQuery() {
+    imdbEL.empty();
     var queryURLmovie = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy"
     $.ajax({
         url: queryURLmovie, //movie ajax
         method: "GET"
+//build response
     }).then(function (response) {
+        console.log(response);
         var newDiv = $("<div>")
         var newH3 = $("<h3>");
         var imdbInfoP = $("<p>");
-        var ratingP = $("<p>");
-        var plotP = $("<p>");
+        var newIMG = $("<img>");
         newH3.text(response.Title)
-        imdbInfoP.text("IMDB rating: " + response.imdbRating + " // votes: " + response.imdbVotes)
-        ratingP.text(response.Rated);
-        plotP.text(response.Plot);
+        imdbInfoP.html("IMDB rating: " + response.imdbRating + " // votes: " + response.imdbVotes + "<br>" + response.Year + "<br>" + response.Rated + "<br>" + response.Plot)
+        newIMG.attr("class", "poster");
+        newIMG.attr("src", response.Poster);
         newDiv.append(newH3);
         newDiv.append(imdbInfoP);
-        newDiv.append(ratingP);
-        newDiv.append(plotP);
+        newDiv.append(newIMG);
         newDiv.addClass("content-box animated slideInRight")
-        $("#corona").append(newDiv)
+        imdbEL.append(newDiv)
     });
 };
 
+//query api to find content and where it's avaiable for streaming
 function streamQuery() {
-
+    streamEL.empty();
     var mediaSettings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + movie + "&country=uk",
+        "url": "https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=" + movie + "&country=us",
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com",
@@ -38,33 +55,28 @@ function streamQuery() {
     };
     $.ajax(mediaSettings).done(function (response) {
         console.log(response)
+        //build response
     }).then(function (response) {
         for (var i = 0; i < response.results.length; i++) {
-            console.log(response.results[i].name);
-            console.log(response.results[i].picture);
-            console.log(response.results[i].locations[0].display_name);
-            console.log(response.results[i].locations[0].url);
             var newDiv = $("<div>")
             var newH3 = $("<h3>");
-            var serviceP = $("<p>");
-            var newImg = $("<img>");
+            var newP = $("<p>");
+            var serviceA = $("<a>");
+            var newIcon = $("<img>")
+            var newImg = $("<img class = 'poster'>");
             newH3.text(response.results[i].name);
-            serviceP.attr("href", response.results[i].locations[0].url);
-            serviceP.text(response.results[i].locations[0].display_name);
+            serviceA.attr("href", response.results[i].locations[0].url);
+            newIcon.attr("src", response.results[i].locations[0].icon);
             newImg.attr("src", response.results[i].picture);
-            newImg.attr("width", "300px");
-            newImg.attr("height", "auto")
             newDiv.append(newH3);
-            newDiv.append(serviceP);
+            serviceA.append(newIcon);
+            newP.append(serviceA);
+            newDiv.append(newP);
             newDiv.append(newImg);
-            // newDiv.append(urlP);
             newDiv.addClass("content-box animated slideInRight")
-            $("#user-search").append(newDiv)
+            streamEL.append(newDiv)
         };
     });
-
-
 };
-
 MDBQuery();
 streamQuery();
